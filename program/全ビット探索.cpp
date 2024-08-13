53718b48862c03944b0cef774adf1765dad0cf4a
tkhs-0114 <tkhs.mail0114@gmail.com>
2024-08-13 15:57:47 +0900

    二分探索の追加

--- program/全ビット探索.cpp
+++ program/全ビット探索.cpp
@@ -1,10 +1,14 @@
 int N=3;
+int A[N] = {1,2,3}
 
 for(int bit =0; bit<(1<<N); bit++){
   for(int i=0; i<N; i++){
     if(bit & (1<<i)){
-      //bitが1ならば
+      cout << A[i];
+    }else{
+      cout << "*";
     }
+    cout << endl;
     //1つの組み合わせ
   }
 }
